1796e6094213f6b3abd3609e56f8d30cb377f2f9
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2024-02-18 11:26:30 +0900

    add toast with fontawesome

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,58 @@
+<!-- src/components/ExampleComponent.vue -->
+<template>
+  <div>
+    <button @click="showToast">Show Toast</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    showToast() {
+      this.$toasted.show(
+        'This is a toast message!', {
+          type: 'error',
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style>
+.toasted-container {
+  /* トーストエリア全体の位置調整 */
+  margin-bottom: -20px; /* 20px下げる */
+  margin-right: -10px; /* 10px右寄せ */
+  max-width: 50%; /* 画面の半分に設定 */
+}
+
+.toasted-container .toasted {
+  /* 各トーストの調整 */
+  min-height: 60px !important;
+  padding-right: 45px !important; /* アイコンの分だけ右を空ける(アイコンをabsoluteにしたための調整) */
+}
+
+/* エラータイプのトーストに適用するスタイル */
+.toasted.error {
+  background-color: #FF367F !important; /* 任意の背景色を指定 */
+  color: #ffffff; /* 任意のテキスト色を指定 */
+}
+
+.white-icon {
+  color: white !important;
+}
+
+.large-icon {
+  font-size: 24px !important;
+}
+
+.toasted-container .icon {
+  /* メッセージが長文で自動改行されない場合、アイコンが追い出されてしまうためabsoluteに設定し、表示位置を固定。 */
+  position: absolute;
+  right: 10px;
+  top: 40%;
+  transform: translateY(-50%);
+  margin-right: 10px
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Toasted from 'vue-toasted'
+import 'vue-toasted/dist/vue-toasted.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Toasted, {
+  position: 'bottom-right',
+  duration: 500000,
+  type: 'info',
+  theme: 'toasted-primary',
+  iconPack : 'fontawesome',
+  action : {
+      text : '',
+      icon : 'fa-times-circle',
+      class: 'white-icon large-icon',
+      onClick : (e, toastObject) => {
+          toastObject.goAway(0);
+      }
+  },
+})
 
 new Vue({
   render: h => h(App),
